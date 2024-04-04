fc5ea6b14451bd0bc4b9e809e42913e354befe1a
SatishSingh0313 <satishsangam.205@gmail.com>
2024-04-04 17:10:44 +0530

    Largest Element in an Array

--- Day 1 - 1. Largest Element in an Array.java
+++ Day 1 - 1. Largest Element in an Array.java
@@ -1,16 +1,14 @@
-//Strivers A2Z DSA Sheet
-
-//Array - Easy
+package day1;
 
 //Largest Element in an Array
 
 /*
 Problem statement:
-Given an array ‘arr’ of size ‘n’ find the largest element in the array.
+Given an array 'Array' of size �n� find the largest element in the array.
 
 Example:
-Input: 'n' = 5, 'arr' = [1, 2, 3, 4, 5]
-Output: 5
+Input: 'n' = 5, 'Array' = [1, 2, 8, 4, 5]
+Output: 8
 
 Explanation: From the array {1, 2, 3, 4, 5}, the largest element is 5. */
 
@@ -18,32 +16,35 @@ import java.util.Scanner;
 
 public class Solution {
 
-    static int largestElement(int[] arr, int n) {
+  static int largestElement(int[] arr, int n) {
 
-        int max = Integer.MIN_VALUE;
+      int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < n; i++) {
+      for (int i = 0; i < n; i++) {
 
-            if (max < arr[i]) {
+          if (max < arr[i]) {
 
-                max = arr[i];
-            }
-        }
+              max = arr[i];
+          }
+      }
 
-        return max;
-    }
+      return max;
+  }
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+	  
+	  System.out.println("Enter Array Size");
+      Scanner sc = new Scanner(System.in);
+      
+      int n = sc.nextInt();
+      System.out.println("Enter "+n+" Number");
+      int arr[] = new int[n];
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+      for (int i = 0; i < n; i++) {
 
-        for (int i = 0; i < n; i++) {
-
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.println(largestElement(arr, n));
-    }
+          arr[i] = sc.nextInt();
+      }
+   
+      System.out.println("Large No :"+largestElement(arr, n));
+  }
 }
