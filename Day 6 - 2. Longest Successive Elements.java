33e296062c0e39e2bab0c8e995045122f9f61a08
SatishSingh0313 <satishsangam.205@gmail.com>
2024-04-11 00:12:36 +0530

    Longest Successive Elements

---
+++ Day 6 - 2. Longest Successive Elements.java
@@ -0,0 +1,60 @@
+package day8;
+
+//Longest Successive Elements
+
+/* Problem statement
+There is an integer array ‘A’ of size ‘N’.
+
+A sequence is successive when the adjacent elements of the sequence have a difference of 1.
+
+You must return the length of the longest successive sequence.
+
+Note:
+You can reorder the array to form a sequence. 
+For example,
+
+Input:
+A = [5, 8, 3, 2, 1, 4], N = 6
+Output: 5
+Explanation: 
+The resultant sequence can be 1, 2, 3, 4, 5.    
+The length of the sequence is 5. */
+
+import java.util.HashSet;
+
+public class Solution {
+	
+    public static int longestSuccessiveElements(int[] a) {
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : a) {
+            set.add(num);
+        }
+
+        int maxLength = 0;
+
+        for (int num : a) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentLength = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentLength++;
+                }
+
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+
+        return maxLength;
+    }
+    
+    public static void main(String[] args) {
+    	
+        int[] array = {5, 8, 3, 2, 1, 4};
+        
+        int longestSequenceLength = Solution.longestSuccessiveElements(array);
+        System.out.println("Longest successive sequence length: " + longestSequenceLength);
+    }
+}
