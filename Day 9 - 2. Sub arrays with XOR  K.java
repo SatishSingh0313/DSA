58170e63b194e55c6c2015faf891f1d634f9b670
SatishSingh0313 <satishsangam.205@gmail.com>
2024-04-12 02:31:58 +0530

    Sub Arrays with XOR ‘K’

---
+++ Day 9 - 2. Sub arrays with XOR  K.java
@@ -0,0 +1,61 @@
+package day11;
+
+
+//Sub arrays with XOR ‘K’
+
+/* Problem statement
+Given an array ‘A’ consisting of ‘N’ integers and an integer ‘B’, 
+find the number of subarrays of array ‘A’ whose bitwise XOR( ⊕ ) of all elements is equal to ‘B’.
+
+A sub array of an array is obtained by removing some(zero or more) elements
+ from the front and back of the array.
+
+Example:
+Input: ‘N’ = 4 ‘B’ = 2
+‘A’ = [1, 2, 3, 2]
+
+Output: 3
+
+Explanation: Sub arrays have bitwise xor equal to ‘2’ are: [1, 2, 3, 2], [2], [2]. */
+
+import java.util.HashMap;
+
+public class Solution {
+	
+    public static int subarraysWithSumK(int[] a, int b) {
+        int n = a.length;
+        int xor = 0;
+        int count = 0;
+        int result = 0;
+
+        // HashMap to store prefix XOR values and their counts
+        HashMap<Integer, Integer> xorCount = new HashMap<>();
+        xorCount.put(0, 1);
+
+        for (int i = 0; i < n; i++) {
+            xor ^= a[i];
+
+            // Calculate the XOR value that needs to be found for a subarray
+            int targetXOR = xor ^ b;
+
+            // If the target XOR is found in the HashMap, add the count to the result
+            if (xorCount.containsKey(targetXOR)) {
+                count += xorCount.get(targetXOR);
+            }
+
+            // Update the count of current XOR value in the HashMap
+            xorCount.put(xor, xorCount.getOrDefault(xor, 0) + 1);
+        }
+
+        return count;
+    }
+    
+    public static void main(String[] args) {
+    	
+        int[] a = {1, 2, 3, 2};
+        int b = 2;
+        
+        int subarraysCount = subarraysWithSumK(a, b);
+        System.out.println("Number of sub Arrays with bitwise XOR equal to " + b + ": " + subarraysCount);
+    }
+}
